0dd3b702d1a0b8fe9a43320480a4a45c04a96f25
celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>
2022-11-09 08:31:46 +0300

    chore : use atexit
    Signed-off-by: celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>

--- Functions/client.cpp
+++ Functions/client.cpp
@@ -1,6 +1,11 @@
 #include "StringVectorHolder.h"
 
+#include <iostream>
 int main() {
     StringVectorHolder h{{"one", "two", "three"}};
     h.print();
+    std::atexit(
+            [](){
+                std::cout << "Exiting string collection example\n";
+            });
 }
